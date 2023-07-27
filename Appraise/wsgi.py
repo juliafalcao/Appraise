9d51537f72f6a26be80516e12ab5d0e503b4751a
Júlia Falcão <juliafsfalcao@hotmail.com>
2023-07-27 13:46:00 +0200

    wsgi.py - load prod config if running in prod server

--- Appraise/wsgi.py
+++ Appraise/wsgi.py
@@ -12,4 +12,13 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Appraise.settings")
 
+# load configs into env
+from configparser import ConfigParser
+cfg = ConfigParser()
+cfg.read("/var/www/rival/public_html/translation-eval/config.ini")
+assert len(cfg), "Config was not loaded correctly"
+
+for key, val in cfg["prod"].items():
+    os.environ[key.upper()] = val
+
 application = get_wsgi_application()  # pylint: disable=invalid-name
